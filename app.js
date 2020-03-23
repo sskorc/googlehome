9f19fa27f0cd0ff417393ff44067f3e6b9f5a5a4
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2020-03-23 12:46:38 +0100

    Add wfh hashtag to response

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-app.get('/', (req, res) => res.send('Hello World! #stayhome'))
+app.get('/', (req, res) => res.send('Hello World! #stayhome #wfh'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
