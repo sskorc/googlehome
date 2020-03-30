7156503be0d3175c02ec3d28d4bf2a2c17711c01
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2020-03-30 10:48:06 +0200

    Fixed parsing error

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
 
-app.post('/', (req, res) => res.send({
-   "fulfillmentText" : req.body.queryResult.parameters["color"]
-  }))
+app.post('/', (req, res) =>{
+  console.log(req.body)
+  res.send({
+    "fulfillmentText" : req.body.queryResult.parameters
+   })
+}
+)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
