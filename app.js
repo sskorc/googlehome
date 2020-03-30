ad6e238c9757b1181fc0268bc8e44238325e21bb
Jakub Mosakowski <mosakowskijakubsm@gmail.com>
2020-03-30 10:38:16 +0200

    Added fetching color parameter from body

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-app.post('/', (req, res) => res.send({ "fulfillmentText" : "Thanks!" }))
+app.post('/', (req, res) => res.send({
+   "fulfillmentText" : req.body.queryResult.parameters["color"]
+  }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
