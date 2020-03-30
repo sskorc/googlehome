892dde6a29b2c1229e927ff2519434ca8367caaf
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2020-03-30 12:18:58 +0200

    Support different cases

--- app.js
+++ app.js
@@ -11,21 +11,35 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-app.post('/', async (req, res) =>{
-  // console.log(req)
+const STATUSES = ['confirmed', 'recovered', 'deaths'];
 
+app.post('/', async (req, res) => {
+  console.log(req.body)
   const country = req.body.queryResult.parameters["geo-country"];
 
-  const response = await axios.get('https://api.covid19api.com/live/country/' + country.toLowerCase() + '/status/confirmed')
+  const casesPromises = STATUSES.map(async status => {
+  	const response = await axios.get('https://api.covid19api.com/live/country/' + country.toLowerCase() + '/status/' + status)
+  	const numberOfCases = response.data.pop().Cases;
 
-  const numberOfCases = response.data.pop().Cases;
+  	return `${status}: ${numberOfCases}`
+  })
+  const numberOfCases = await Promise.all(casesPromises)
+
+  console.log(numberOfCases)
+
+  const numberOfCasesText = `The current numbers for ${country} are ${numberOfCases.join(', ')}`
+  console.log(numberOfCasesText)
+
+  // const response = await axios.get('https://api.covid19api.com/live/country/' + country.toLowerCase() + '/status/confirmed')
+
+  // const numberOfCases = response.data.pop().Cases;
 
   // const numberOfCases = 100;
 
   res.send({
-    "fulfillmentText" : `The current number of cases in ${country} is ${numberOfCases}`
+    "fulfillmentText" : numberOfCasesText
+    //`The current number of cases in ${country} is ${numberOfCases}`
    })
-}
-)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
