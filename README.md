080ee2282c1ed91f734be170a7b1ad68dd357d9d
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2020-05-27 14:17:35 +0200

    formatting and README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Doctor Coder - back-end
+This is the back-end part of the **Docter Coder** app available for Google Assistant.
+
+Doctor Coder gives you daily statistics of COVID-19 in different countries.
+
+It uses [COVID19API](https://covid19api.com/) as a data source.
+
+### Demo
+
+[![Doctor Coder - demo](http://img.youtube.com/vi/VNBudRXqc9c/0.jpg)](http://www.youtube.com/watch?v=VNBudRXqc9c "Doctor Coder - demo")
+
+### Authors
+
+I was proudly developed in a mob programming fashion by:
+
+- [JakubMosakowski](https://github.com/JakubMosakowski)
+- [kitek](https://github.com/kitek)
+- [mtinn](https://github.com/mtinn)
+- [solariss13](https://github.com/solariss13)
+- [sskorc](https://github.com/sskorc)
--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 let port = process.env.PORT;
@@ -16,20 +16,22 @@ const STATUSES = ['confirmed', 'recovered', 'deaths'];
 app.post('/', async (req, res) => {
   console.log(req.body)
   const country = req.body.queryResult.parameters["geo-country"];
-  let countrySlug = country.replace(/\'/,'').replace(/\s+/g, '-').toLowerCase()
-    if(countrySlug == 'south-korea') {
-        countrySlug = 'korea-south'
-    }
-    else if(countrySlug == 'united-states') {
-        countrySlug = 'us'
-    }
-  const casesPromises = STATUSES.map(async status => {
-  	try {
-  	  const response = await axios.get('https://api.covid19api.com/total/country/' + countrySlug + '/status/' + status)
-  	  const numberOfCases = response.data.pop().Cases;
+  let countrySlug = country.replace(/\'/, '').replace(/\s+/g, '-').toLowerCase()
+  
+  if (countrySlug == 'south-korea') {
+    countrySlug = 'korea-south'
+  }
+  else if (countrySlug == 'united-states') {
+    countrySlug = 'us'
+  }
 
-  	  return `${status}: ${numberOfCases}`
-    } catch(e) {
+  const casesPromises = STATUSES.map(async status => {
+    try {
+      const response = await axios.get('https://api.covid19api.com/total/country/' + countrySlug + '/status/' + status)
+      const numberOfCases = response.data.pop().Cases;
+
+      return `${status}: ${numberOfCases}`
+    } catch (e) {
       return undefined
     }
   })
@@ -37,25 +39,16 @@ app.post('/', async (req, res) => {
 
   let numberOfCasesText
   if (numberOfCases.some(element => element == undefined)) {
-  	numberOfCasesText = `Sorry, I don't understand: ${country}. Please provide another name.`;
+    numberOfCasesText = `Sorry, I don't understand: ${country}. Please provide another name.`;
   } else {
-  	numberOfCasesText = `The current numbers for ${country} are ${numberOfCases.join(', ')}.`
+    numberOfCasesText = `The current numbers for ${country} are ${numberOfCases.join(', ')}.`
   }
-
-
 
   console.log(numberOfCasesText)
 
-  // const response = await axios.get('https://api.covid19api.com/live/country/' + country.toLowerCase() + '/status/confirmed')
-
-  // const numberOfCases = response.data.pop().Cases;
-
-  // const numberOfCases = 100;
-
   res.send({
-    "fulfillmentText" : numberOfCasesText
-    //`The current number of cases in ${country} is ${numberOfCases}`
-   })
+    "fulfillmentText": numberOfCasesText
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
